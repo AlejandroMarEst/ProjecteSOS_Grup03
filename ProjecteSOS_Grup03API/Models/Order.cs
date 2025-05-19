b96db5322e02db8e7a4c914c4b7c86c03133e5d1
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-05-19 12:29:56 +0200

    feature(api): Created models. Closes #1

---
+++ ProjecteSOS_Grup03API/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjecteSOS_Grup03API.Models
+{
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderId { get; set; }
+
+        public string? ClientId { get; set; }
+        public Client Client { get; set; }
+
+        public Employee? SalesRep { get; set; }
+        public string? SalesRepId { get; set; }
+
+        public double Price { get; set; }
+
+        public ICollection<ProductOrder> OrdersProducts { get; set; }
+    }
+}
