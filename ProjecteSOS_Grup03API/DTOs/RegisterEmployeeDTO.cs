418239b82ccec6379555797821d90e165bcd2336
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-05-20 08:22:31 +0200

    feature(api): Created authentication controller. Closes #2

---
+++ ProjecteSOS_Grup03API/DTOs/RegisterEmployeeDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjecteSOS_Grup03API.DTOs
+{
+    public class RegisterEmployeeDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public DateOnly? StartDate { get; set; }
+        public double Salary { get; set; }
+        public string? ManagerId { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
