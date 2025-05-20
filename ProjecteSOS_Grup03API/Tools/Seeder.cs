718908a747eb460dae2e8b167b590ab6f42949fd
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-05-20 10:18:48 +0200

    feature(api): created admin seed

--- ProjecteSOS_Grup03API/Tools/Seeder.cs
+++ ProjecteSOS_Grup03API/Tools/Seeder.cs
@@ -19,5 +19,31 @@ namespace ProjecteSOS_Grup03API.Tools
                 }
             }
         }
+        public static async Task SeedAdmins(IServiceProvider services)
+        {
+            var userManager = services.GetRequiredService<UserManager<User>>();
+
+            var admin = new Employee
+            {
+                Name = "Admin1",
+                UserName = "admin@dynamx.com",
+                Email = "admin@dynamx.com",
+                Salary = 3000,
+                ManagerId = null,
+                IsAdmin = true,
+                StartDate = DateOnly.FromDateTime(DateTime.Now),
+                PhoneNumber = "167890"
+            };
+            string password = "Admin1234!";
+
+            if (await userManager.FindByEmailAsync(admin.Email) == null)
+            {
+                var result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "Admin");
+                }
+            }
+        }
     }
 }
