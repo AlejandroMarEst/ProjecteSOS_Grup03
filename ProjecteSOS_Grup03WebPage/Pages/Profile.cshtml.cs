cd0d3a221e5868a92476d570983f4def3bd8207d
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-05-21 13:02:25 +0200

    feature(web): created users profile page (unfinished) Waiting for api method to get current user profile information

---
+++ ProjecteSOS_Grup03WebPage/Pages/Profile.cshtml.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjecteSOS_Grup03WebPage.DTOs;
+using System.Net.Http;
+using System.Text.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ProjecteSOS_Grup03WebPage.Pages
+{
+    public class ProfileModel : PageModel
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILogger<ProfileModel> _logger;
+
+        public UserProfileDTO? Profile { get; set; }
+        public string? ErrorMessage { get; set; }
+
+        public ProfileModel(IHttpClientFactory httpClient, ILogger<ProfileModel> logger)
+        {
+            _httpClientFactory = httpClient;
+            _logger = logger;
+        }
+
+        public async Task OnGetAsync()
+        {
+            try
+            {
+                var client = _httpClientFactory.CreateClient("SosApi");
+                var response = await client.GetAsync("api/");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    Profile = JsonSerializer.Deserialize<UserProfileDTO>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                    if (Profile == null)
+                    {
+                        ErrorMessage = "No s'ha trobat l'usuari.";
+                    }
+                }
+                else
+                {
+                    _logger.LogError("User Profile Loading Failed");
+                    ErrorMessage = "Loading Profile Error";
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error Loading User Profile");
+                ErrorMessage = "There was an unexpected error. Try again.";
+            }
+        }
+    }
+}
