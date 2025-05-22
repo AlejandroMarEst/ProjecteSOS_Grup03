841c3b56d06ca4f38ce37a8c8835a0466196e7d4
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-05-22 09:00:22 +0200

    feature(api): Each user can now see their orders

---
+++ ProjecteSOS_Grup03WebPage/DTOs/OrderDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjecteSOS_Grup03WebPage.DTOs
+{
+    public class OrderDTO
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } // Nom del producte per referència
+        public int Quantity { get; set; }
+        public DateTime OrderDate { get; set; }
+        public double UnitPrice { get; set; } // Preu del producte en el moment
+        public double TotalPrice => UnitPrice * Quantity;
+    }
+}
