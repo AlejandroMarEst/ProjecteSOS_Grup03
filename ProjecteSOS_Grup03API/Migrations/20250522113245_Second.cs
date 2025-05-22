f62bc33b89a82a73b4e7fbb6708965242c854850
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-05-22 13:33:17 +0200

    feature(api): migration for order new property

---
+++ ProjecteSOS_Grup03API/Migrations/20250522113245_Second.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjecteSOS_Grup03API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
