ae70d9f6a8bf13598e0d791e28b164d035555ae7
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-05-26 10:36:05 +0200

    feature(web): created shoping cart add products

--- ProjecteSOS_Grup03WebPage/DTOs/ProductListDTO.cs
+++ ProjecteSOS_Grup03WebPage/DTOs/ProductListDTO.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
+        public int Stock { get; set; }
         public double Price { get; set; }
     }
 }
