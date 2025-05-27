ace4ea5893a33f25906dd4ec9af8e0c3364fe6ac
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-05-27 09:44:58 +0200

    feature(api): created method to get current order product

---
+++ ProjecteSOS_Grup03API/DTOs/ProductOrderDTO.cs
@@ -0,0 +1,12 @@
+﻿using ProjecteSOS_Grup03API.Models;
+
+namespace ProjecteSOS_Grup03API.DTOs
+{
+    public class ProductOrderDTO
+    {
+        public DateTime OrderDate { get; set; }
+
+        public int Quantity { get; set; }
+
+    }
+}
