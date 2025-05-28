b856eb1df3cddb7cd682fd3618209e7078d2428d
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-05-28 08:42:14 +0200

    Feature(web): Created page for admins to see all users and check their stats

---
+++ ProjecteSOS_Grup03WebPage/Pages/Employees/Stats.cshtml.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using ProjecteSOS_Grup03WebPage.DTOs;
+using ProjecteSOS_Grup03WebPage.Pages.Orders;
+using ProjecteSOS_Grup03WebPage.Tools;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace ProjecteSOS_Grup03WebPage.Pages.Employees
+{
+    public class StatsModel : PageModel
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILogger<OrderListModel> _logger;
+        public List<OrderListDTO> Orders { get; set; } = [];
+        public List<UserProfileDTO> Users { get; set; } = [];
+        public string? ErrorMessage { get; set; }
+        public StatsModel(IHttpClientFactory httpClientFactory, ILogger<OrderListModel> logger)
+        {
+            _httpClientFactory = httpClientFactory;
+            _logger = logger;
+        }
+        public async Task OnGetAsync()
+        {
+            try
+            {
+                var client = _httpClientFactory.CreateClient("SosApi");
+                var token = HttpContext.Session.GetString("AuthToken");
+
+                if (TokenHelper.IsTokenSession(token))
+                {
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                }
+                var responseOrders = await client.GetAsync("api/Orders");
+                var responseUsers = await client.GetAsync("api/Auth/Profiles");
+                if (responseOrders.IsSuccessStatusCode)
+                {
+                    var json = await responseOrders.Content.ReadAsStringAsync();
+                    var orders = JsonSerializer.Deserialize<List<OrderListDTO>>(json, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+                    var json2 = await responseUsers.Content.ReadAsStringAsync();
+                    var users = JsonSerializer.Deserialize<List<UserProfileDTO>>(json2, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+                    Orders = orders ?? new List<OrderListDTO>();
+                    Users = users ?? new List<UserProfileDTO>();
+                }
+                else
+                {
+                    _logger.LogError(await responseOrders.Content.ReadAsStringAsync());
+                    ErrorMessage = "Loading Orders Error";
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error Loading Orders");
+                ErrorMessage = "There was an unexpected error. Try again.";
+            }
+        }
+    }
+}
