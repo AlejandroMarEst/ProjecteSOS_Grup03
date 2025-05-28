37e997bf9a2ed50c0d14f26e16d5826c7c0ed4af
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-05-28 10:32:12 +0200

    feature(api): added data validation to dtos classes

--- ProjecteSOS_Grup03API/DTOs/UserProfileDTO.cs
+++ ProjecteSOS_Grup03API/DTOs/UserProfileDTO.cs
@@ -1,10 +1,23 @@
-﻿namespace ProjecteSOS_Grup03API.DTOs
+﻿using ProjecteSOS_Grup03API.Tools;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjecteSOS_Grup03API.DTOs
 {
     public class UserProfileDTO
     {
+        [Required(ErrorMessage = ValidationMessages.EmailRequired)]
+        [EmailAddress(ErrorMessage = ValidationMessages.EmailInvalid)]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.NameRequired)]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = ValidationMessages.NameLength)]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.PhoneRequired)]
+        [Phone(ErrorMessage = ValidationMessages.PhoneInvalid)]
         public string Phone { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = ValidationMessages.PointsPositive)]
         public int? Points { get; set; }
         public DateOnly? StartDate { get; set; }
         public bool? IsAdmin { get; set; }
